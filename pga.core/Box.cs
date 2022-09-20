7f5c5f6e7e9eb538e04711b28e78c6660bc028b9
maceoneh <davidmorenomagin@gmail.com>
2022-09-20 17:59:43 +0200

    Desarrollando

--- pga.core/Box.cs
+++ pga.core/Box.cs
@@ -192,6 +192,7 @@ namespace pga.core
                     await permissionshelper.BuildAsync();
                     //Se crean las tablas que contienen los permisos de los registros
                     await permissionshelper.CreateTablePermissions<DTOBoxMessage>();
+
                     //Se crean los permisos
                     var e_appointment = await permissionshelper.AddEntityAsync("appointment");
                     var e_subject = await permissionshelper.AddEntityAsync("subject");
